37d6fcce45b0830fff7608b45c55da74a35c985d
syl-gauthier <sylvain.gauthier.2012@gmail.com>
2017-10-25 15:27:54 +0200

    Twitter api usage draft

--- app.js
+++ app.js
@@ -3,9 +3,12 @@
 
 "use strict";
 
+require("dotenv").config();
+
 const express = require("express");
 const morgan = require("morgan");
 
+const port = process.env.PORT||3000;
 
 const app = express();
 
@@ -13,7 +16,24 @@ app.use(morgan("tiny"));
 
 app.use(express.static("public"));
 
-const port = process.env.PORT||3000;
+
+const Twitter = require("twitter");
+
+console.log(typeof Twitter);
+
+var client = new Twitter({
+  consumer_key: process.env.TWITTER_CONS_KEY,
+  consumer_secret: process.env.TWITTER_CONS_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
+const retweet = require("retweet")(client);
+
+
+retweet.get("919262509227323392", {style: "tiny"}).then(function(result) {
+  console.log(result);
+});
 
 app.listen(port, function() {
   console.log("\x1b[32m", "app listening on port", port, "\x1b[0m");
