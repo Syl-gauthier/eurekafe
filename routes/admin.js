9c3d7257581888f38168efb5ffef9a1dc07584a5
syl-gauthier <sylvain.gauthier.2012@gmail.com>
2017-11-05 13:38:35 +0100

    Twitter and facebook API first draft

--- routes/admin.js
+++ routes/admin.js
@@ -20,7 +20,8 @@ router.use(passport.initialize());
 router.use(passport.session());
 
 passport.serializeUser(function(user, cb) {
-  //console.log (user, "serialized");
+  console.log(user);
+  require("../lib/facebook.js")(user.accessToken);
   return cb(null, user);
 });
 passport.deserializeUser(function(user, done) {
@@ -35,16 +36,23 @@ passport.use(new facebookStrat({
   profileFields: ["id", "displayName", "photos", "email"]
 },
 function(accessToken, refreshToken, profile, cb) {
+  profile.accessToken = accessToken;
+  profile.refreshToken = refreshToken;
   cb(null, profile);
 }));
 
-router.get("/", passport.authenticate("facebook", {scope: ["email"]}));
+router.get("/", passport.authenticate("facebook", {scope: ["email", "manage_pages", "publish_pages"]}));
 
 router.get("/auth/callback", passport.authenticate("facebook", {failureRedirect: "fail"}), 
   function(req, res) {
     res.redirect("success");
   }
 );
+
+router.get("/auth/failure", function(req, res) {
+  //loginFailure redirect to public website after 5sec
+  res.render("loginFailure");
+});
 
 //after this, user need to be logged in
 router.use(function(req, res, next) {
@@ -56,13 +64,38 @@ router.use(function(req, res, next) {
   }
 });
 
+router.get("/auth/success", function(req, res) {
+  //redirect to /admin/dashboard after 1sec
+  res.render("loginSuccess");
+});
+
+router.get("/dashboard", function(req, res) {
+  res.render("adminDashboard");
+});
+
 router.get("/publish", function(req, res) {
   res.render("publish");
 });
 
 router.post("/publish", function(req, res) {
-  console.log(req.body);
   res.send("oui");
+});
+
+router.get("/twitterFeed", function(req, res) {
+  var twitter = require("../lib/twitter.js")();
+  twitter.getFeed("AOswd", function(err, tweets) {
+    if(err) console.log(err);
+    res.send(tweets);
+  });
+});
+
+router.get("/facebookFeed", function(req, res) {
+  console.log(typeof req.user);
+  var fb = require("../lib/facebook.js")(req.user.accessToken);
+  fb.getFeed(function(err, response) {
+    if(err) console.log(err);
+    res.send(response);
+  });
 });
 
 module.exports = router;
